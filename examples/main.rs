11aef6b95e67d0735986204385653753a01f5bc3
shockham <samuel.hockham@gmail.com>
2017-07-31 17:04:05 +0100

    Fix main example for api update

--- examples/main.rs
+++ examples/main.rs
@@ -1,12 +1,17 @@
 extern crate volition;
+extern crate winit;
 
 use volition::Input;
+use winit::{ EventsLoop, Window };
 
 fn main() {
-    let mut input = Input::new(); 
+    let events_loop = EventsLoop::new();
+    let window = Window::new(&events_loop).unwrap();
+
+    let mut input = Input::from_existing(events_loop);
 
     for _ in 0..10 {
-        input.update_inputs();
+        input.update_inputs(&window);
         println!("{:?}", input.keys_down);
     }
 }
