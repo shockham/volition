380ff9ee691d2eeb4214656620b9dcfe0ca2a887
shockham <samuel.hockham@gmail.com>
2017-11-21 13:24:22 +0000

    Update main example to be more useful, sleeping between updating inputs

--- examples/main.rs
+++ examples/main.rs
@@ -4,6 +4,9 @@ extern crate winit;
 use volition::Input;
 use winit::{EventsLoop, Window};
 
+use std::thread;
+use std::time::Duration;
+
 fn main() {
     let events_loop = EventsLoop::new();
     let window = Window::new(&events_loop).unwrap();
@@ -11,6 +14,7 @@ fn main() {
     let mut input = Input::from_existing(events_loop);
 
     for _ in 0..10 {
+        thread::sleep(Duration::new(1u64, 0u32));
         input.update_inputs(&window);
         println!("{:?}", input.keys_down);
     }
